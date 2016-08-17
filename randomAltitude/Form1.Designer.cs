66242727eb86889d4d0d3ed6aa57107706a6723a
Razdavitel <razdavitel@protonmail.com>
2016-08-17 20:50:14 +0200

    Start project

---
+++ randomAltitude/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace randomAltitude
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBaseAlt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblAlt = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbHigh = new System.Windows.Forms.RadioButton();
+            this.rbMedium = new System.Windows.Forms.RadioButton();
+            this.rbFlat = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBaseAlt
+            // 
+            this.txtBaseAlt.Location = new System.Drawing.Point(17, 39);
+            this.txtBaseAlt.Name = "txtBaseAlt";
+            this.txtBaseAlt.Size = new System.Drawing.Size(75, 20);
+            this.txtBaseAlt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Base altitude";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(17, 75);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(17, 102);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblAlt
+            // 
+            this.lblAlt.AutoSize = true;
+            this.lblAlt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlt.Location = new System.Drawing.Point(286, 77);
+            this.lblAlt.Name = "lblAlt";
+            this.lblAlt.Size = new System.Drawing.Size(57, 31);
+            this.lblAlt.TabIndex = 4;
+            this.lblAlt.Text = ". . .";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbFlat);
+            this.groupBox1.Controls.Add(this.rbMedium);
+            this.groupBox1.Controls.Add(this.rbHigh);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.txtBaseAlt);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 142);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbHigh
+            // 
+            this.rbHigh.AutoSize = true;
+            this.rbHigh.Location = new System.Drawing.Point(109, 59);
+            this.rbHigh.Name = "rbHigh";
+            this.rbHigh.Size = new System.Drawing.Size(47, 17);
+            this.rbHigh.TabIndex = 6;
+            this.rbHigh.TabStop = true;
+            this.rbHigh.Text = "High";
+            this.rbHigh.UseVisualStyleBackColor = true;
+            // 
+            // rbMedium
+            // 
+            this.rbMedium.AutoSize = true;
+            this.rbMedium.Location = new System.Drawing.Point(109, 82);
+            this.rbMedium.Name = "rbMedium";
+            this.rbMedium.Size = new System.Drawing.Size(62, 17);
+            this.rbMedium.TabIndex = 7;
+            this.rbMedium.TabStop = true;
+            this.rbMedium.Text = "Medium";
+            this.rbMedium.UseVisualStyleBackColor = true;
+            // 
+            // rbFlat
+            // 
+            this.rbFlat.AutoSize = true;
+            this.rbFlat.Location = new System.Drawing.Point(109, 105);
+            this.rbFlat.Name = "rbFlat";
+            this.rbFlat.Size = new System.Drawing.Size(42, 17);
+            this.rbFlat.TabIndex = 8;
+            this.rbFlat.TabStop = true;
+            this.rbFlat.Text = "Flat";
+            this.rbFlat.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 177);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblAlt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBaseAlt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblAlt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbFlat;
+        private System.Windows.Forms.RadioButton rbMedium;
+        private System.Windows.Forms.RadioButton rbHigh;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ randomAltitude/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace randomAltitude
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbFlat.Checked = true;
+        }
+        //global
+        bool timerRunning = false;
+        int baseAlt = 0;
+        Random rand = new Random();
+        private void btnStart_Click(object sender, EventArgs e)
+        {           
+            //altitude
+            if (txtBaseAlt.Text == "" || txtBaseAlt.Text == null)
+                MessageBox.Show("Textbox requires value.", "failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else {
+                baseAlt = int.Parse(txtBaseAlt.Text);
+                txtBaseAlt.Enabled = false;
+                btnStart.Enabled = false;
+                timerRunning = true;
+                timer1.Start();   
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        { //all done
+            txtBaseAlt.Enabled = true;
+            btnStart.Enabled = true;
+            txtBaseAlt.Clear();
+            lblAlt.Text = "0";
+            timer1.Stop();
+            timerRunning = false;
+        }
+        public void refreshAltitude(int baseAlt)
+        {//all done
+            int altitude = 0;
+            int deviation = 0;
+            if (rbFlat.Checked == true)
+                deviation = rand.Next(1, 10);
+            if (rbMedium.Checked == true)
+                deviation = rand.Next(1, 15);
+            if (rbHigh.Checked == true)
+                deviation = rand.Next(1, 25);
+            altitude = rand.Next(baseAlt, baseAlt+deviation);
+            lblAlt.Text = altitude.ToString();
+            //return altitude;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timerRunning == true) { 
+            refreshAltitude(baseAlt);
+                int randTime = rand.Next(1000, 2000);
+                timer1.Interval = randTime;
+                timer1.Start();
+            }
+        }
+    }
+}
